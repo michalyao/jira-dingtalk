852a7529f50bd795740eacdc68f3de40ecae599e
michalyao <yaoyao0777@gmail.com>
2016-12-31 18:31:04 +0800

    test: 测试lombok创建的JavaBean

---
+++ dingtalkjiraplugin/src/test/java/me/yoryor/plugin/client/Test.java
@@ -0,0 +1,9 @@
+package me.yoryor.plugin.client;
+
+import io.vertx.core.json.JsonObject;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(new JsonObject(""));
+    }
+}
---
+++ dingtalkjiraplugin/src/test/java/me/yoryor/plugin/entity/OaMessageTest.java
@@ -0,0 +1,12 @@
+package me.yoryor.plugin.entity;
+
+import org.junit.Test;
+
+public class OaMessageTest {
+    @Test
+    public String toString() {
+//        OaMessage oaMessage = OaMessage.builder().body()
+        return null;
+    }
+
+}
