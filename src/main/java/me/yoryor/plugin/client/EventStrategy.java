9b36b4ca0b723fc6867389b378a6db4d43059e54
michalyao <yaoyao0777@gmail.com>
2017-01-10 16:06:44 +0800

    update: 以JsonObject的形式作为函数的输入

--- src/main/java/me/yoryor/plugin/client/EventStrategy.java
+++ src/main/java/me/yoryor/plugin/client/EventStrategy.java
@@ -1,6 +1,7 @@
 package me.yoryor.plugin.client;
 
 import com.google.common.collect.Lists;
+import io.vertx.core.json.JsonObject;
 import me.yoryor.plugin.entity.*;
 
 import java.time.format.DateTimeFormatter;
@@ -29,9 +30,12 @@ public class EventStrategy {
     private static final String ISSUE_COMMENT_EDITED = "issue_comment_edited";
     private static final String ISSUE_REOPENED = "issue_reopened";
     private static final String ISSUE_DELETED = "issue_deleted";
+    private static final String ISSUE_ASSIGNED = "issue_assigned";
+
+
     private static final String ISSUE_GENERIC = "issue_generic";
 
-    private final Map<String, Function<Issue, Message>> eventFuncMap = new HashMap<>();
+    private final Map<String, Function<JsonObject, Message>> eventFuncMap = new HashMap<>();
 
     private EventStrategy() {
         super();
@@ -39,20 +43,45 @@ public class EventStrategy {
     }
 
     private void initMap() {
-        // 输入: ClientVerticle 接收到消息解析出的issue实体
+        // 输入: ClientVerticle 从eventbus接收到的消息
         // 输出: DingDing发送的消息体
-        Function<Issue, Message> issueCreated = this::issueCreatedMsg;
+        Function<JsonObject, Message> issueCreated = this::issueCreatedMsg;
+        Function<JsonObject, Message> issueAssigned = this::issueAssignedMsg;
+
+
         eventFuncMap.put(ISSUE_CREATED, issueCreated);
+        eventFuncMap.put(ISSUE_ASSIGNED, issueAssigned);
     }
 
-    private Message issueCreatedMsg(Issue issue) {
+    // 只能指定唯一一个经办人
+    private Message issueAssignedMsg(JsonObject wrapJson) {
+        Issue issue = Issue.fromJson(wrapJson.getJsonObject("issue"));
+        JsonObject changelog = wrapJson.getJsonObject("changelog");
+        ChangeItem item = ChangeItem.fromJson(changelog.getJsonArray("items").getJsonObject(0));
+        Form from = new Form("From: ", item.getFrom());
+        Form to = new Form("To: ", item.getTo());
+        MessageBody messageBody = MessageBody.builder()
+                .content(String.format("%s 更新了任务的经办人", issue.getUser()))
+                .form(Lists.newArrayList(from, to))
+                .title("Monitor/" + issue.getId()) // TODO: 2017/1/6 get name from board or project.
+                .build();
+        MessageHead messageHead = MessageHead.builder()
+                .bgcolor("FFDC123B")
+                .text("")
+                .build();
+        return  Message.builder().url("http://www.uyun.cn")
+                .body(messageBody)
+                .head(messageHead)
+                .build();
+    }
+
+    private Message issueCreatedMsg(JsonObject wrapJson) {
+        Issue issue = Issue.fromJson(wrapJson.getJsonObject("issue"));
         Form type = new Form("问题类型: ", issue.getType());
         Form version = new Form("影响版本: ", issue.getVersions().toString());
         Form assignee = new Form("经办人: ", issue.getAssignee());
         Form reporter = new Form("报告人: ", issue.getReporter());
-        // FIXME: 2017/1/6 make me readable
         Form created = new Form("创建时间: ", issue.getCreated().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss")));
-
         Form priority = new Form("优先级: ", issue.getPriority());
 
         MessageBody messageBody = MessageBody.builder()
@@ -71,8 +100,8 @@ public class EventStrategy {
                 .build();
     }
 
-    public Function<Issue, Message> getFuncByEventType(String eventType) {
-        Function<Issue, Message> function = eventFuncMap.get(eventType);
+    public Function<JsonObject, Message> getFuncByEventType(String eventType) {
+        Function<JsonObject, Message> function = eventFuncMap.get(eventType);
         if (function != null) {
             return function;
         }
