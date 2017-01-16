d76371ed430cdeda7bb02d0eea7cf1ad01ffaaf4
michalyao <yaoyao0777@gmail.com>
2017-01-16 18:28:15 +0800

    update: 使用更加规范的异常类

--- src/main/java/me/yoryor/plugin/client/DingDingException.java
+++ src/main/java/me/yoryor/plugin/client/DingDingException.java
@@ -1,6 +1,14 @@
 package me.yoryor.plugin.client;
 
 public class DingDingException extends RuntimeException {
+    public DingDingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DingDingException(Throwable cause) {
+        super(cause);
+    }
+
     public DingDingException(String message) {
         super(message);
     }
